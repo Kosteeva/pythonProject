cb78372fa22df29c5209b0998a7025882432a3cd
Julia <kosteevajulia@outlook.com>
2022-11-22 18:52:32 +0300

    Problem solving, content independence, looking for a bug

--- pages/locators.py
+++ pages/locators.py
@@ -15,4 +15,16 @@ class LoginPageLocators:
     REG_PSW2 = (By.NAME, 'registration-password2')
 
 
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CLASS_NAME, 'btn-add-to-basket')
+    BUTTON_VIEW_BASKET = (By.CSS_SELECTOR, '.btn-group > .btn')
+    BOOK_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
+    ALERT_MESSAGE = (By.CSS_SELECTOR, "div.alert:nth-child(1) strong")
+    ALERT_BOOK_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    ADDED_BOOK_NAME = (By.CSS_SELECTOR, '.col-sm-4 a')
+    ADDED_BOOK_PRICE = (By.CSS_SELECTOR, '.col-sm-1 p')
+
+
+
 
---
+++ pages/product_page.py
@@ -0,0 +1,47 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_product = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        add_product.click()
+        self.solve_quiz_and_get_code()
+        self.should_be_message_contain_book_name()
+        self.should_be_message_contain_the_same_price()
+
+    def go_to_basket(self):
+        basket = self.browser.find_element(*ProductPageLocators.BUTTON_VIEW_BASKET)
+        basket.click()
+
+    def should_be_message_contain_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.ALERT_MESSAGE).text
+        #print(book_name, message)
+        assert book_name == message, "This book doesn't added in the basket"
+
+    def should_be_message_contain_the_same_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        price_message = self.browser.find_element(*ProductPageLocators.ALERT_BOOK_PRICE).text
+        #print(book_price, price_message)
+        assert book_price == price_message, "Price doesn't match"
+
+    def should_be_book(self):
+        book_name = self.is_element_present(*ProductPageLocators.BOOK_NAME)
+        assert book_name, "No name book"
+
+    def should_be_price(self):
+        book_price = self.is_element_present(*ProductPageLocators.BOOK_PRICE)
+        assert book_price, "No price"
+
+    def should_be_book_added(self):
+        book_name = self.is_element_present(*ProductPageLocators.ADDED_BOOK_NAME)
+        assert book_name, "No name added book"
+
+    def should_be_price_added(self):
+        book_price = self.is_element_present(*ProductPageLocators.ADDED_BOOK_PRICE)
+        assert book_price, "No added price"
+
+
+
+
